b6c3049d185ff46b034fc9efdfcee002fef7b965
candale <candale.andrei@gmail.com>
2016-08-16 16:04:09 +0300

    Added initial device model

--- server/mqtt/device/models.py
+++ server/mqtt/device/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    device_id = models.CharField(
+        'Unique string that represents the device',
+        max_length=256, db_index=True, unique=True)
+    spec = models.TextField('Exposed operations specifications')
+    online = models.BooleanField(
+        'The online/offline state of the device', default=False)
+    last_online = models.DateTimeField(
+        'Last time when the device was online', null=True, blank=True)
